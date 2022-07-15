ca7a21a1a1d37cc366e2ae2f77486262fd5433fe
Jeff Paredes <>
2022-07-15 10:08:46 +0000

    🚧 added expected constants

--- src/game.py
+++ src/game.py
@@ -2,3 +2,9 @@
 The application is a simulation of a toy robot moving on a square table top
 '''
 
+KEYWORDS = ('PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT')
+DIRECTIONS = ('NORTH', 'EAST', 'SOUTH', 'WEST')
+TABLE_DIMENSIONS = (5, 5)
+X_DIRECTIONS = ('NORTH', 'SOUTH')
+Y_DIRECTIONS = ('EAST', 'WEST')
+
